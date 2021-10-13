a98205e6323f62ecb9a030f27d3e606fde4a4cd5
mehmetA58 <mehmet.akbayir58@gmail.com>
2021-10-13 13:58:25 +0300

    v13/10

--- src/test/java/tests/day08/C_HomeWork.java
+++ src/test/java/tests/day08/C_HomeWork.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -25,9 +26,7 @@ public class C_HomeWork {
      * Verify item prices are sorted from low to high
      */
     /**
-     * https://www.saucedemo.com/ adresine gidin
-     * Kullanıcı adını standard_user olarak girin
-     * Şifreyi secret_sauce olarak girin
+
      * Giriş düğmesine tıklayın
      * Düşükten yükseğe fiyatı seçin
      * Ürün fiyatlarının düşükten yükseğe doğru sıralandığını doğrulayın
@@ -45,50 +44,48 @@ public class C_HomeWork {
     }
     @Test
     public void test () throws InterruptedException {
+        // * https://www.saucedemo.com/ adresine gidin
+        //     * Kullanıcı adını standard_user olarak girin
+        //     * Şifreyi secret_sauce olarak girin
+
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        //Click on login button
         driver.findElement(By.id("login-button")).click();
-      driver.findElement(By.xpath("(//div[@class='inventory_item_price'])"));
 
-        List<WebElement> Sirasizvalues = new ArrayList<>();
-        List<WebElement> Sirasizelements =driver.findElements(By.xpath("(//div[@class='inventory_item_price'])"));
-        for (int i=0;i<Sirasizelements.size();i++) {
-            Sirasizvalues.add(Sirasizelements.get(i));
-        }
-        for(int i=0;i<Sirasizvalues.size();i++){
-            System.out.println(Sirasizvalues.get(i).getText());
-        }
+        //* Choose price low to high
+        //     * Verify item prices are sorted from low to high
 
-        System.out.println("======================================================================================");
-Thread.sleep(3000);
-        driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div[2]/span/select/option[3]")).click();
-Thread.sleep(3000);
-        List<WebElement> values = new ArrayList<>();
-        List<WebElement> elements =driver.findElements(By.xpath("(//div[@class='inventory_item_price'])"));
-        for (int i=0;i<elements.size();i++) {
-            values.add((elements.get(i)));
-        }
-        for(int i=0;i<values.size();i++){
-            System.out.println(values.get(i).getText());
+        WebElement dropDown=driver.findElement(By.className("product_sort_container"));
+        Select select=new Select(dropDown);
+        select.selectByValue("lohi");
+        List<WebElement> fiyatlar=driver.findElements(By.className("inventory_item_price"));
+        List<String> fiyatlarString=new ArrayList<>();
+
+        fiyatlar.stream().forEach(t->fiyatlarString.add(t.getText()));
+
+        List<Double> fiyatlarDouble=new ArrayList<>();
+
+        for (int i = 0; i < fiyatlarString.size() ; i++) {
+            fiyatlarDouble.add(Double.parseDouble(fiyatlarString.get(i).replace("$","")));
         }
 
-
-        if (Sirasizvalues.equals(values)){
-            System.out.println("Test FAİLED");
-        }else {
-            System.out.println("Test PASS");
+        for (int i = 1; i < fiyatlarDouble.size() ; i++) {
+            if (fiyatlarDouble.get(i-1)<=fiyatlarDouble.get(i)){
+                System.out.println("Test PASS");
+            }else {
+                System.out.println("Test FAİLED");
+            }
         }
+        System.out.println("fiyatlar : "+fiyatlar.toString());
+        System.out.println("fiyatlarString"+fiyatlarString);
+        System.out.println("fiyatlarDouble"+fiyatlarDouble);
 
     }
-
-
-
-
-
     @AfterClass
     public void tearDown() {
 
-        driver.quit();
+       driver.quit();
     }
 }
