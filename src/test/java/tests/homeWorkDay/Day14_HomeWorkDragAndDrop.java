0751786510cbad7a30816aaa010d2f60fbb39e15
mehmetA58 <mehmet.akbayir58@gmail.com>
2021-10-21 00:39:35 +0300

    day14 homeWork added

--- src/test/java/tests/homeWorkDay/Day14_HomeWorkDragAndDrop.java
+++ src/test/java/tests/homeWorkDay/Day14_HomeWorkDragAndDrop.java
@@ -35,33 +35,39 @@ public class Day14_HomeWorkDragAndDrop {
         @Test
         public void test() {
             driver.get("http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html");
+
             WebElement oslo=driver.findElement(By.id("box1"));
             WebElement stockholm=driver.findElement(By.id("box2"));
             WebElement washington=driver.findElement(By.id("box3"));
-            WebElement copenhang=driver.findElement(By.id("box4"));
+            WebElement copenhagen=driver.findElement(By.id("box4"));
             WebElement seoul=driver.findElement(By.id("box5"));
             WebElement rome=driver.findElement(By.id("box6"));
             WebElement madrid=driver.findElement(By.id("box7"));
+
             WebElement norway=driver.findElement(By.id("box101"));
             WebElement sweden=driver.findElement(By.id("box102"));
-            WebElement unitedStates =driver.findElement(By.id("box103"));
+            WebElement unitedStates=driver.findElement(By.id("box103"));
             WebElement denmark=driver.findElement(By.id("box104"));
             WebElement sKorea=driver.findElement(By.id("box105"));
-            WebElement ıtaly=driver.findElement(By.id("box106"));
+            WebElement italy=driver.findElement(By.id("box106"));
             WebElement spain=driver.findElement(By.id("box107"));
 
             Actions actions=new Actions(driver);
-            actions.dragAndDrop(oslo,norway).perform();
-            actions.dragAndDrop(stockholm,sweden).perform();
-            actions.dragAndDrop(washington,unitedStates).perform();
-            actions.dragAndDrop(copenhang,denmark).perform();
-            actions.dragAndDrop(seoul,sKorea).perform();
-            actions.dragAndDrop(rome,ıtaly).perform();
-            actions.dragAndDrop(madrid,spain).perform();
+             actions.dragAndDrop(oslo,norway)
+                     .dragAndDrop(stockholm,sweden)
+                     .dragAndDrop(washington,unitedStates)
+                     .dragAndDrop(copenhagen,denmark)
+                     .dragAndDrop(seoul,sKorea)
+                     .dragAndDrop(rome,italy)
+                     .dragAndDrop(madrid,spain).perform();
+
+
+
+
         }
         @AfterClass
     public void tearDown(){
-           // driver.quit();
+            driver.quit();
         }
         }
 
